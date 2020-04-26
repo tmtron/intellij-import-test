b482351c56f4f971275f7e5cd37d415db8f40a38
TmTron <martin.trummer@tmtron.com>
2020-04-26 09:44:59 +0200

    added some comments on new branch

--- index.ts
+++ index.ts
@@ -1,6 +1,15 @@
 import {snakeCase} from "snake-case";
 
+/**
+ * note: the snake-case package is explicitly imported in package.json: auto-import works
+ */
 const x = snakeCase('snakeCase');
+
+/**
+ * note: the camel-case package is NOT explicitly imported in package.json: auto-import does not work, even though
+ * we import change-case
+ */
+const camel = camelCase
 
 interface  Foo {
     bar:String;
